74cf7ac4fecc7987e517c50dae5ef1031a029191
Antonio Aleman <antonio.9714@gmail.com>
2019-02-28 21:32:11 -0600

    lol

--- node/app.js
+++ node/app.js
@@ -1,1 +1,13 @@
 console.log("lmao");
+
+const fs = require('fs');
+const ejemplo = require('./ejemplo');
+
+const _= require('lodash');
+
+var arr = ['Andres', 'Enrique', 'Juanpi'];
+
+console.log(_.reverse(arr));
+
+//console.log(ejemplo.imprimeHola());
+//console.log(ejemplo.multiplica(2, 3));
---
+++ node/ejemplo.js
@@ -0,0 +1,7 @@
+module.exports.imprimeHola = function() {
+    return 'HOLA';
+}
+
+module.exports.multiplica = function(n1, n2) {
+    return n1*n2;
+}
