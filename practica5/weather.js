3d7ca34b464ee455def579a1836767942e341f23
Antonio Aleman <antonio.9714@gmail.com>
2019-03-18 18:18:26 -0600

    lol

--- practica5/weather.js
+++ practica5/weather.js
@@ -4,6 +4,7 @@
 const request = require('request');
 const credentials = require('./credentials.js');
 
+/*
 const getCoordinates = function(parameter) {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ parameter + '.json?access_token=' + credentials.coordKey;
     request({url: url, json: true}, function(error, response) {
@@ -22,9 +23,29 @@ const getForecast = function(coord) {
         console.log('Actualmente esta a ' + data.temperature + '°C' + ' y ' + data.summary + '. Hay un ' + data.precipProbability + '% de probablidad de lluvia.');
     });
 }
+*/
+
+function getForecast (coord) {
+    const url = 'https://api.darksky.net/forecast/' + credentials.darkskyKey + '/' + coord + '?exclude=[minutely, hourly, daily]&lang=es&units=si';
+    request({url: url, json: true}, function(error, response) {
+        const data = response.body.currently;
+        console.log('Actualmente esta a ' + data.temperature + '°C' + ' y ' + data.summary + '. Hay un ' + data.precipProbability + '% de probablidad de lluvia.');
+    });
+}
+
+function getCoordinates (callback, parameter) {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ parameter + '.json?access_token=' + credentials.coordKey;
+    request({url: url, json: true}, function(error, response) {
+        //console.log(response.body.features[0].center[0]);
+        const latitude = response.body.features[0].center[1];
+        const longitude = response.body.features[0].center[0];
+        const coordinates = latitude + ',' + longitude;
+        callback(coordinates);
+    });
+}
 
 const getWeather = function(city) {
-    getCoordinates(city);
+    getCoordinates(getForecast, city);
 }
 
 module.exports = {
