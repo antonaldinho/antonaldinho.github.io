f47439c1c196e7bc51f72890e6d0b27bb3fc508a
Antonio Aleman <antonio.9714@gmail.com>
2019-05-13 15:20:00 -0500

    changed base url

--- democracyproject/js/editProfile.js
+++ democracyproject/js/editProfile.js
@@ -1,4 +1,4 @@
-const base = 'http://127.0.0.1:5500/frontEnd/html/';
+const base = 'https://antonaldinho.github.io/democracyproject/html/';
 const userData = JSON.parse($.session.get("userData"));
 const token = $.session.get("token");
 
